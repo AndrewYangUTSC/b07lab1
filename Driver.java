44b2a4c2890160b59e9d66436e0cf86c7cc9d87e
AndrewYang <andrewm.yang@mail.utoronto.ca>
2022-10-03 23:26:11 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,53 @@
-public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
+import java.io.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Test polynomial constructor using file argument
+		//Commented out because files are local
+		/*File f = new File("C:\\Users\\andyy\\b07lab1\\test.txt");
+		Polynomial p = new Polynomial(f);
+		p.saveToFile("poly.txt");*/
+		
+		//Addition test
+		double[] coeff1 = new double[] {4, 5, 7};
+		int[] exp1 = new int[] {0, 1, 2};
+		Polynomial p1 = new Polynomial(coeff1, exp1);
+		
+		double[] coeff2 = new double[] {6, -2, 5};
+		int[] exp2 = new int[] {0, 1, 3};
+		Polynomial p2 = new Polynomial(coeff2, exp2);
+		
+		Polynomial sum = p1.add(p2);
+		System.out.println("Sum coefficients:");
+		for(int i = 0; i < sum.coeff.length; i++) {
+			System.out.print(sum.coeff[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Sum exponents:");
+		for(int i = 0; i < sum.exp.length; i++) {
+			System.out.print(sum.exp[i] + " ");
+		}
+		System.out.println();
+		
+		//Multiply test
+		Polynomial prod = p1.multiply(p2);
+		System.out.println("Product coefficients:");
+		for(int i = 0; i < prod.coeff.length; i++) {
+			System.out.print(prod.coeff[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Product exponents:");
+		for(int i = 0; i < prod.exp.length; i++) {
+			System.out.print(prod.exp[i] + " ");
+		}
+		System.out.println();
+		
+		//Evaluate test
+		System.out.println("Evaluation of p1 with x=1: " + p1.evaluate(1));
+
+	}
+
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,47 +1,199 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Polynomial {
 	double[] coeff;
+	int[] exp;
 
 	public Polynomial() {
-		//Initialize array [0]
-		coeff = new double[]{0};
+		//Initialize both arrays as null
+		coeff = null;
+		exp = null;
 	}
 
-	public Polynomial(double[] p) {
-		//Initialize array with the elements of p
-		coeff = p;
+	public Polynomial(double[] c, int[] e) {
+		//Initialize coeff and exp arrays with the elements of c and e respectively
+		coeff = c;
+		exp = e;
+	}
+	
+	public Polynomial(File f) {
+		try {
+			Scanner sc = new Scanner(f);
+			String old_str = sc.nextLine();
+			
+			//Add plus signs in front of all minus signs to make it easier to split into terms
+			String new_str = old_str.replaceAll("\\-", "\\+-");
+			//Remove empty first element if it exists
+			if(new_str.charAt(0) == '+') {
+				new_str = new_str.substring(1, new_str.length());
+			}
+			
+			//Split polynomial into terms
+			String poly_terms[] = new_str.split("\\+");
+			coeff = new double[poly_terms.length];
+			exp = new int[poly_terms.length];
+			
+			//For each term, parse coefficient and exponent
+			for(int i = 0; i < poly_terms.length; i++) {
+				String terms[] = poly_terms[i].split("x");
+				if(poly_terms[i].contains("x")) {
+					coeff[i] = Double.parseDouble(terms[0]);
+					exp[i] = Integer.parseInt(terms[1]);
+				}
+				else {
+					coeff[i] = Double.parseDouble(terms[0]);
+					exp[i] = 0;
+				}
+			}
+			
+			//For zero polynomial, set arrays to null
+			if(old_str == "0") {
+				coeff = null;
+				exp = null;
+			}
+			
+			sc.close();
+		}
+		
+		catch(IOException e) {
+			System.out.println("File reading error!");
+		}
 	}
 
-	public Polynomial add(Polynomial p) {
-		
-		int len;
-		Polynomial sum;
-		
-		//Case 1: The passed-in polynomial is longer
-		if(this.coeff.length < p.coeff.length) {
-			sum = new Polynomial(p.coeff);
-			len = this.coeff.length;
+	public Polynomial add(Polynomial p) { 
+		//If either polynomial is zero, return the other
+		if(this.coeff == null) {
+			return p;
 		}
-		//Case 2: The passed-in polynomial is not longer
-		else {
-			sum = new Polynomial(this.coeff);
-			len = p.coeff.length;
+		if(p.coeff == null) {
+			return this;
 		}
 		
-		//Add the shorter polynomial's coefficients to sum, which is the polynomial with the coefficients of the longer one
-		for(int i = 0; i < len; i++) {
-			sum.coeff[i] = this.coeff[i] + p.coeff[i];
+		//Add coefficients of corresponding exponents using temp array
+		int this_deg = p.getDegree(this.exp);
+		int p_deg = p.getDegree(p.exp);
+		int deg = Math.max(this_deg, p_deg);
+		double[] temp = new double[deg + 1];
+		
+		for(int i = 0; i < temp.length; i++) {
+			temp[i] = 0;
 		}
 		
+		for(int i = 0; i < this.exp.length; i++) {
+			temp[this.exp[i]] += this.coeff[i];
+		}
+		
+		for(int i = 0; i < p.exp.length; i++) {
+			temp[p.exp[i]] += p.coeff[i];
+		}
+		
+		int nonzero_count = 0;
+		for(int i = 0; i < temp.length; i++) {
+			if(temp[i] != 0) {
+				nonzero_count++;
+			}
+		}
+		
+		double[] sum_coeff = new double[nonzero_count];
+		int[] sum_exp = new int[nonzero_count];
+		
+		//Remove all entries from temp that have coefficients of zero
+		int index = 0;
+		for(int i = 0; i < temp.length; i++) {
+			if(temp[i] != 0) {
+				sum_coeff[index] = temp[i];
+				sum_exp[index] = i;
+				index++;
+			}
+		}
+		
+		if(sum_coeff.length == 0) {
+			sum_coeff = null;
+			sum_exp = null;
+		}
+		
+		Polynomial sum = new Polynomial(sum_coeff, sum_exp);
 		return sum;
+	}
+	
+	public Polynomial multiply(Polynomial p) {
+		if(this.coeff == null) {
+			return p;
+		}
+		if(p.coeff == null) {
+			return this;
+		}
 		
+		int this_deg = p.getDegree(this.exp);
+		int p_deg = p.getDegree(p.exp);
+		double[] temp = new double[this_deg + p_deg + 1];
+		
+		for(int i = 0; i < temp.length; i++) {
+			temp[i] = 0;
+		}
+		
+		//Multiply coefficients and add exponents
+		for(int i = 0; i < this.exp.length; i++) {
+			for(int j = 0; j < p.exp.length; j++) {
+				int exp = this.exp[i] + p.exp[j];
+				temp[exp] += this.coeff[i] * p.coeff[j];
+			}
+		}
+		
+		int nonzero_count = 0;
+		for(int i = 0; i < temp.length; i++) {
+			if(temp[i] != 0) {
+				nonzero_count++;
+			}
+		}
+		
+		double[] prod_coeff = new double[nonzero_count];
+		int[] prod_exp = new int[nonzero_count];
+		
+		//Remove all entries from temp that have coefficients of zero
+		int index = 0;
+		for(int i = 0; i < temp.length; i++) {
+			if(temp[i] != 0) {
+				prod_coeff[index] = temp[i];
+				prod_exp[index] = i;
+				index++;
+			}
+		}
+		
+		if(prod_coeff.length == 0) {
+			prod_coeff = null;
+			prod_exp = null;
+		}
+		
+		Polynomial prod = new Polynomial(prod_coeff, prod_exp);
+		return prod;
+		
+	}
+	
+	public int getDegree(int[] arr) {
+		int degree = arr[0];
+		
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] > degree) {
+				degree = arr[i];
+			}
+		}
+		
+		return degree;
 	}
 
 	public double evaluate(double x) {
+		//Zero polynomial will always evaluate to 0
+		if(coeff == null) {
+			return 0;
+		}
+		
 		double evaluation = 0;
 
 		//Evaluate by summing up the product of the coefficient and the passed-in value raised to the exponent i
 		for(int i = 0; i < this.coeff.length; i++) {
-			evaluation += this.coeff[i] * Math.pow(x, i);
+			evaluation += this.coeff[i] * Math.pow(x, this.exp[i]);
 		}
 		
 		return evaluation;
@@ -54,5 +206,45 @@ public class Polynomial {
 		}
 
 		return false;
+	}
+	
+	public void saveToFile(String file_name) {
+		try {
+			File f = new File("C:\\Users\\andyy\\b07lab1\\" + file_name);
+			FileWriter fw = new FileWriter(f);
+			
+			//Write 0 in the case of a zero polynomial
+			if(coeff == null) {
+				fw.write("0");
+				fw.close();
+				return;
+			}
+			
+			String p = "";
+			for(int i = 0; i < exp.length; i++) {
+				if(coeff[i] > 0) {
+					p += "+" + Double.toString(coeff[i]);
+				}
+				else {
+					p += Double.toString(coeff[i]);
+				}
+			
+				if(exp[i] > 0) {
+					p += "x" + Integer.toString(exp[i]);			
+				}	
+			}
+		
+			if(p.charAt(0) == '+') {
+				p = p.substring(1, p.length());
+			}
+		
+			fw.write(p);
+			fw.close();
+			
+		}
+		catch(IOException e) {
+			System.out.println("File writing error!");
+		}
+		
 	}
 }
